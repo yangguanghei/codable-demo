70c30392441deb26aa3f33ea980443785f1b8623
liangsen2 <liangsende1@dingtalk.com>
2020-11-06 19:04:42 +0800

    Initial Commit

---
+++ codable使用/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  codable使用
+//
+//  Created by 梁森 on 2020/11/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
